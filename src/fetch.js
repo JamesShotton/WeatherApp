0ee7700a7ad877d7f0824ba947bb312aad006ba2
JamesShotton <james.shotton73@gmail.com>
2024-12-26 01:37:00 +0000

    landing done not responsive

--- src/fetch.js
+++ src/fetch.js
@@ -4,17 +4,7 @@ const apiKey = process.env.API_KEY;
 const city = "London";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error. Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    console.log(data.weather);
-  })
-  .catch((error) => {
-    console.log(`Error fetching weather data: ${error}`);
-  });
+async function checkWeather() {
+  const response = await fetch(url);
+  const data = await response.json();
+}
